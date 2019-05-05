06ce775a6f151440c82e76e72cfdd6d91fb5a700
himadri-repo <himadri.trainer@gmail.com>
2019-05-05 16:42:57 +0530

    Admin Portal - NG7 version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {HttpClientModule} from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UsersComponent } from './components/users/users.component';
+import { HeaderbarComponent } from './headerbar/headerbar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UsersComponent,
+    HeaderbarComponent
   ],
   imports: [
     BrowserModule,
